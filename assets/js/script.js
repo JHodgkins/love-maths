e5529b8301e1f9a48b6d36dfe040ef930e7b7cc3
JHodgkins <jhodgkins78@gmail.com>
2022-04-03 00:15:04 +0000

    code: DOM content loaded & event listeners added to game buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+// Wait for the DOM to loadd before running the game
+
+// add event listeners to each of the buttons
+document.addEventListener('DOMContentLoaded', function(){
+    let buttons = document.getElementsByTagName('button');
+
+    for (let button of buttons){
+        button.addEventListener('click', function(){
+           if (this.getAttribute('data-type') === 'submit') {
+               alert('you pressed submit');
+           }else {
+               let gameType = this.getAttribute('data-type');
+               alert(`You selected ${gameType}`);
+           }
+        });
+    }
+});
+
 function runGame(){
 
 }
