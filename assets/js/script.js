17cdce60fab25c082535024e66edbeaaed07f36c
JHodgkins <jhodgkins78@gmail.com>
2022-04-04 17:24:42 +0000

    code: added variables to create random numbers between 1 & 25

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,9 +15,14 @@ document.addEventListener('DOMContentLoaded', function(){
         });
     }
 });
-
+/**
+ * Main game "loop" run when the script is first loaded
+ * and after the user's answer has been processed.
+ */
 function runGame(){
-
+    // Generate 2 random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswer(){
@@ -51,4 +56,3 @@ function displayMultiplyQuestion(){
 function displayDivisionQuestion(){
 
 }
-
