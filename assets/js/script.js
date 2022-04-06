1467ef90168e92cef7d1b83f796f7acc5f3b106e
JHodgkins <jhodgkins78@gmail.com>
2022-04-06 08:31:22 +0000

    code: Division functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,8 @@ function runGame(gameType){
         displaySubtractQuestion(num1,num2);
     }else if (gameType === 'multiply') {
         displayMultiplyQuestion(num1,num2);
+    }else if(gameType === 'division') {
+        displayDivisionQuestion(num1,num2);
     } else {
         alert(`Unknown Game Type: ${gameType}`);
         throw `Unknown Game Type: ${gameType}.abort game`;
@@ -68,6 +70,8 @@ function calculateCorrectAnswer(){
         return [operand1 - operand2, "subtract"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
+    }else if(operator === "/") {
+        return [operand1 / operand2, "division"];
     } else {
         alert("Unimplimented operator: ${operator}");
         throw `Unimplimented operator: ${operator}.aborting`;
@@ -106,6 +110,8 @@ function displayMultiplyQuestion(operand1, operand2){
     document.getElementById('operator').textContent = 'x';
 }
 
-function displayDivisionQuestion(){
-
+function displayDivisionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = '/';
 }
