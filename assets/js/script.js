f7b229e4341d36f2424386b924f4ea2a9f2109b4
JHodgkins <jhodgkins78@gmail.com>
2022-04-06 08:17:41 +0000

    code: subtract functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,6 +26,8 @@ function runGame(gameType){
     let num2 = Math.floor(Math.random() * 25) + 1;
     if (gameType === 'addition') {
         displayAdditionQuestion(num1,num2);
+    }else if (gameType === 'subtract') {
+        displaySubtractQuestion(num1,num2);
     }else if (gameType === 'multiply') {
         displayMultiplyQuestion(num1,num2);
     } else {
@@ -62,7 +64,9 @@ function calculateCorrectAnswer(){
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
-    }else if (operator === "x") {
+    }else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
+    } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
     } else {
         alert("Unimplimented operator: ${operator}");
@@ -90,8 +94,10 @@ function displayAdditionQuestion(operand1,operand2){
     document.getElementById('operator').textContent = '+';
 }
 
-function displaySubtractQuestion(){
-
+function displaySubtractQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = '-';
 }
 
 function displayMultiplyQuestion(operand1, operand2){
