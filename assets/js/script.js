b3fc6582799c396b2ffd12448fdf625b2774f343
JHodgkins <jhodgkins78@gmail.com>
2022-04-06 04:41:15 +0000

    code: checkAnswer functionality implimented

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function(){
     for (let button of buttons){
         button.addEventListener('click', function(){
            if (this.getAttribute('data-type') === 'submit') {
-               alert('you pressed submit');
+               checkAnswer();
            }else {
                let gameType = this.getAttribute('data-type');
                runGame(gameType);
@@ -31,9 +31,21 @@ function runGame(gameType){
         throw `Unknown Game Type: ${gameType}.abort game`;
     }
 }
-
+/**
+ * Checks if the answer against the first element in the returned calculateCorrectAnswer
+ * array
+ */
 function checkAnswer(){
+    let userAnswer = parseInt(document.getElementById('answer-box').value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
 
+    if (isCorrect) {
+        alert (`Well Document, You got the answer correct! :D`);
+    } else {
+        alert (`Snaps, Thant's incorrect, You answered ${userAnswer}. - The correct answer was ${calculatedAnswer}`);
+    }
+    runGame(calculatedAnswer[1]);
 }
 /**
  * Get the operands (numbers) and the operator (plus, minus, multiply, division)
