b6db960b65c16d71067f0b66ffa1731c5a9803cf
JHodgkins <jhodgkins78@gmail.com>
2022-04-06 03:25:50 +0000

    code: addition display logic added

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,19 +10,26 @@ document.addEventListener('DOMContentLoaded', function(){
                alert('you pressed submit');
            }else {
                let gameType = this.getAttribute('data-type');
-               alert(`You selected ${gameType}`);
+               runGame(gameType);
            }
         });
     }
+    runGame('addition');
 });
 /**
  * Main game "loop" run when the script is first loaded
  * and after the user's answer has been processed.
  */
-function runGame(){
+function runGame(gameType){
     // Generate 2 random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+    if (gameType === 'addition') {
+        displayAdditionQuestion(num1,num2);
+    } else {
+        alert(`Unknown Game Type: ${gameType}`);
+        throw `Unknown Game Type: ${gameType}.abort game`;
+    }
 }
 
 function checkAnswer(){
@@ -41,8 +48,10 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
-
+function displayAdditionQuestion(opperand1,opperand2){
+    document.getElementById('operand1').textContent = opperand1;
+    document.getElementById('operand2').textContent = opperand2;
+    document.getElementById('operator').textContent = '+';
 }
 
 function displaySubtractQuestion(){
